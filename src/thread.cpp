0c6ff7c2430c568408eb9b89faee78133faa1f07
letusget <a14705129921@163.com>
2024-05-25 17:38:31 +0800

    feat: 信号量操作

--- src/thread.cpp
+++ src/thread.cpp
@@ -13,6 +13,59 @@ static thread_local std::string t_current_thread_name = "UNKNOWN";
 // TODO static初始化问题
 static mylog::Logger::ptr g_logger = MYLOG_LOG_NAME("system");
 
+Semaphore::Semaphore(uint32_t count) {
+    if (sem_init(&m_semaphore, 0, count)) {
+        throw std::logic_error("sem_init failed");
+        MYLOG_LOG_ERROR(g_logger) << "sem_init failed, error code: " << errno;
+    }
+}
+
+Semaphore::~Semaphore() {}
+
+// 等待信号量
+void Semaphore::wait() {
+    // while (true)
+    // {
+    //     // sem_wait返回0表示获取成功，返回-1表示获取失败，errno=EINTR表示被信号打断，需要重试
+    //     if(!sem_wait(&m_semaphore)) {
+    //         return;
+    //     }
+    //     // 重试
+    //     if(errno!= EINTR) {
+    //         MYLOG_LOG_WARN(g_logger) << "sem_wait failed, retrying..., error code: " << errno;
+    //         throw std::logic_error("sem_wait failed");
+    //     }
+    // }
+
+    // sem_wait返回0表示获取成功，返回-1表示获取失败，
+    while (sem_wait(&m_semaphore)) {
+        // errno=EINTR表示被信号打断，需要重试
+        if (errno != EINTR) {
+            MYLOG_LOG_WARN(g_logger) << "sem_wait failed, retrying..., error code: " << errno;
+            throw std::logic_error("sem_wait failed");
+        }  
+    }
+}
+
+// 提醒信号量
+void Semaphore::notify() {
+    // if (sem_post(&m_semaphore)) {
+    //     MYLOG_LOG_ERROR(g_logger) << "sem_post failed, error code: " << errno;
+    //     throw std::logic_error("sem_post failed");
+    // }
+
+    // sem_post 返回0表示获取成功，返回-1表示获取失败，
+    while (sem_post(&m_semaphore))
+    {
+        // errno=EINTR表示被信号打断，需要重试
+        if (errno != EINTR) {
+            MYLOG_LOG_WARN(g_logger) << "sem_post failed, retrying..., error code: " << errno;
+            throw std::logic_error("sem_post failed");
+        }
+    }
+    
+}
+
 // 获取当前线程对象指针
 Thread* Thread::GetThis() { return t_thread; }
 
@@ -35,12 +88,15 @@ Thread::Thread(std::function<void()> cb, const std::string& name) {
         m_name = name;
     }
     m_cb = std::move(cb);
-    
+
     int rt = pthread_create(&m_pthread_id, nullptr, &Thread::run, this);
     if (rt != 0) {
         MYLOG_LOG_ERROR(g_logger) << "pthread_create failed, error code: " << rt << " name: " << name;
         throw std::logic_error("pthread_create failed");
     }
+
+    // 等待线程启动
+    m_semaphore.wait();
 }
 
 Thread::~Thread() {
@@ -64,17 +120,22 @@ void Thread::join() {
 void* Thread::run(void* arg) {
     Thread* thread = static_cast<Thread*>(arg);
     // 设置线程局部存储变量
-    t_thread = thread;
+    t_thread              = thread;
     t_current_thread_name = thread->m_name;
-    thread->m_id = mylog::GetThreadId();
+    thread->m_id          = mylog::GetThreadId();
     // 设置线程名称 pthread_setname_np最大支持16个字符
     pthread_setname_np(pthread_self(), thread->m_name.substr(0, 15).c_str());
 
     std::function<void()> cb;
     // 防止智能指针没有释放
     cb.swap(thread->m_cb);
+
+    // 通知线程启动完成
+    thread->m_semaphore.notify();
+
+    // 执行回调函数
     cb();
-    return nullptr; 
+    return nullptr;
 }
 
 }  // namespace myserver
--- src/thread.h
+++ src/thread.h
@@ -5,6 +5,8 @@
  * 使用C++11支持的std::thread
  */
 #include <pthread.h>
+#include <semaphore.h>
+#include <stdint.h>
 
 #include <functional>
 #include <memory>
@@ -14,6 +16,39 @@
 // typedef pthread_t tid_t;
 namespace myserver {
 
+// 封装信号量对象
+class Semaphore {
+   public:
+    Semaphore(uint32_t count = 0);
+    ~Semaphore();
+
+    // 等待信号量
+    void wait();
+    // 提醒信号量
+    void notify();
+    // // 释放信号量
+    // void post();
+    // // 获取信号量计数
+    // uint32_t getValue() const;
+    // // 设置信号量计数
+    // void setValue(uint32_t count);
+    // // 重置信号量计数
+    // void reset(uint32_t count = 0);
+    // // 等待信号量，直到超时或计数为0    
+    // bool timedWait(uint32_t timeout_ms);
+
+   private:
+    // 禁用拷贝构造函数和赋值运算符，防止信号量对象被复制
+    Semaphore(const Semaphore&)  = delete;
+    Semaphore(const Semaphore&&) = delete;
+    Semaphore& operator=(const Semaphore&) = delete;
+    Semaphore& operator=(const Semaphore&&) = delete;
+
+   private:
+    sem_t m_semaphore;
+};
+
+// 封装线程对象
 class Thread {
    public:
     typedef std::shared_ptr<Thread> ptr;
@@ -32,6 +67,7 @@ class Thread {
     static const std::string& GetCurrentThreadName();
     // 设置当前线程名称
     static void SetCurrentThreadName(const std::string& name);
+
    private:
     // 禁用拷贝构造函数和赋值运算符，防止线程对象被复制
     Thread(const Thread&)  = delete;
@@ -50,6 +86,9 @@ class Thread {
     std::function<void()> m_cb = nullptr;
     // 线程名称
     std::string m_name = "";
+
+    // 信号量对象
+    Semaphore m_semaphore;
 };
 
 }  // namespace myserver
